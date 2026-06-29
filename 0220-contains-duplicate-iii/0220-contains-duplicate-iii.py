8f21e595d118baed5dc034bd55704e26e843fcc8
Arpit Jat <arpitjat1903@gmail.com>
2026-06-30 02:12:32 +0530

    Time: 135 ms (72.81%), Space: 37.2 MB (54.1%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -1,18 +1,24 @@
-from sortedcontainers import SortedList
-
 class Solution:
-    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
-        window = SortedList()
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if t < 0:
+            return False
+
+        w = t + 1
+        buckets = {}
 
         for i, num in enumerate(nums):
-            pos = window.bisect_left(num - valueDiff)
+            b = num // w
 
-            if pos < len(window) and abs(window[pos] - num) <= valueDiff:
+            if b in buckets:
+                return True
+            if b - 1 in buckets and abs(num - buckets[b - 1]) < w:
+                return True
+            if b + 1 in buckets and abs(num - buckets[b + 1]) < w:
                 return True
 
-            window.add(num)
+            buckets[b] = num
 
-            if len(window) > indexDiff:
-                window.remove(nums[i - indexDiff])
+            if i >= k:
+                del buckets[nums[i - k] // w]
 
         return False
