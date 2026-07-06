bfcdb31534a08ea6f293067610a1c21b4aa7ce3c
Arpit Jat <arpitjat1903@gmail.com>
2026-07-06 21:40:33 +0530

    Time: 0 ms (100%), Space: 19.5 MB (16.48%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        parent = list(range(26))
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        for eq in equations:
+            if eq[1:3] == "==":
+                union(ord(eq[0]) - ord("a"), ord(eq[3]) - ord("a"))
+
+        for eq in equations:
+            if eq[1:3] == "!=":
+                if find(ord(eq[0]) - ord("a")) == find(ord(eq[3]) - ord("a")):
+                    return False
+
+        return True
