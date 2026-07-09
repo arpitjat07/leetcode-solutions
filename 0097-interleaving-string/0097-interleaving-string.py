34b99d5259b700e6f4e5c4369f4a4ffe917415b4
Arpit Jat <arpitjat1903@gmail.com>
2026-07-09 22:21:21 +0530

    Time: 51 ms (58.49%), Space: 19.2 MB (90.78%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,18 +1,24 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
+        m, n = len(s1), len(s2)
+
+        if m + n != len(s3):
             return False
 
-        m, n = len(s1), len(s2)
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
+        if n > m:
+            s1, s2 = s2, s1
+            m, n = n, m
 
-        for i in range(m + 1):
-            for j in range(n + 1):
-                if i > 0 and s1[i - 1] == s3[i + j - 1]:
-                    dp[i][j] |= dp[i - 1][j]
+        dp = [False] * (n + 1)
+        dp[0] = True
 
-                if j > 0 and s2[j - 1] == s3[i + j - 1]:
-                    dp[i][j] |= dp[i][j - 1]
+        for j in range(1, n + 1):
+            dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
 
-        return dp[m][n]
+        for i in range(1, m + 1):
+            dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
+            for j in range(1, n + 1):
+                dp[j] = (dp[j] and s1[i - 1] == s3[i + j - 1]) or \
+                        (dp[j - 1] and s2[j - 1] == s3[i + j - 1])
+
+        return dp[n]
